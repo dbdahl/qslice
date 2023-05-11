d84e94956eadd4a829875c0fd03f55464883a5d2
smbjohnson <smbjohnson98@gmail.com>
2023-05-11 13:20:50 -0600

    Adding in files needed to test curve 3

--- sam_comparison/curve3/curve3_setup.R
+++ sam_comparison/curve3/curve3_setup.R
@@ -19,7 +19,7 @@ dev.off()
 
 grid <- seq(from = 0,
             to = qgamma(.99999, shape = 2.5, rate = 1),
-            length.out = 1000)
+            length.out = 5000)
 
 # value for Kullback-Leibler Divergence
 py <- exp(lf(grid))
@@ -39,7 +39,7 @@ s <- c(3)#c(3, 5, 10)#c(0.01, 1, 2, 10)
 rate <- c(0.1, 0.5, 2, 5)#c(0.5, 1, 1.5, 2, 2.5, 3)
 
 ## gess slice sampling metrics to input ##
-mu <- c(3, 5, 10)#c(1,2,3,4.5,6,7)
+mu <- c(1, 3, 5, 10)#c(1,2,3,4.5,6,7)
 sigma <- c(1, 3, 5)#c(2,3,4,5,6,8)
 df <- c(3, 5, 10)#c(1,4,16,16^2,16^4)
 
@@ -51,7 +51,7 @@ log_pdf <- c(function(x) dgamma(x, shape = 2.5, rate = 1, log = TRUE),
              laplace_approximation$log_pdf,
              function(x) dnorm(x, mean = 4, sd = 4, log = TRUE),
              function(x) dgamma(x, shape = 2.5, rate = 2, log = TRUE),
-             function(x) dgamma(x, shape = 2.5, rate = 3, log = TRUE),
+             function(x) dgamma(x, shape = 2.5, rate = 0.5, log = TRUE),
              function(x) dgamma(x, shape = 10, rate = 4, log = TRUE),
              function(x) dgamma(x, shape = 3, rate = 1, log = TRUE),
              function(x) dgamma(x, shape = 3.5, rate = 1, log = TRUE),
@@ -63,27 +63,12 @@ inv_cdf <- c(function(u) qgamma(u, shape = 2.5, rate = 1),
              laplace_approximation$inv_cdf,
              function(u) qnorm(u, mean = 4, sd = 4),
              function(u) qgamma(u, shape = 2.5, rate = 2),
-             function(u) qgamma(u, shape = 2.5, rate = 3),
+             function(u) qgamma(u, shape = 2.5, rate = 0.5),
              function(u) qgamma(u, shape = 10, rate = 4),
              function(u) qgamma(u, shape = 3, rate = 1),
              function(u) qgamma(u, shape = 3.5, rate = 1),
              function(u) qgamma(u, shape = 4, rate = 1)
 )
-
-find_grid <- list(seq(from = 0, to = qgamma(0.99999, shape = 2.5, rate = 1), length.out = 1000),
-                  seq(from = qnorm((1-.99999)/2, mean = 2, sd = 4),
-                      to = qnorm((1-.99999)/2, mean = 2, sd = 4, lower.tail = FALSE), length.out = 1000),
-                  seq(from = laplace_approximation$inv_cdf((1-0.99999)/2),
-                      to = laplace_approximation$inv_cdf((1-0.99999)/2, lower.tail = FALSE), length.out = 1000),
-                  seq(from = qnorm((1-.99999)/2, mean = 4, sd = 4),
-                      to = qnorm((1-.99999)/2, mean = 4, sd = 4, lower.tail = FALSE), length.out = 1000),
-                  seq(from = 0, to = qgamma(0.99999, shape = 2.5, rate = 2), length.out = 1000),
-                  seq(from = 0, to = qgamma(0.99999, shape = 2.5, rate = 3), length.out = 1000),
-                  seq(from = 0, to = qgamma(0.99999, shape = 10, rate = 4), length.out = 1000),
-                  seq(from = 0, to = qgamma(0.99999, shape = 3, rate = 1), length.out = 1000),
-                  seq(from = 0, to = qgamma(0.99999, shape = 3.5, rate = 1), length.out = 1000),
-                  seq(from = 0, to = qgamma(0.99999, shape = 4, rate = 1), length.out = 1000)
-                  )
 
 ## random walk tuning parameters ##
 c <- c(1, 2, 5, 10)
--- sam_comparison/curve3/curve3_transform.R
+++ sam_comparison/curve3/curve3_transform.R
@@ -7,7 +7,7 @@ source('curve3_setup.R')
 
 px <- data.frame(px = matrix(nrow = length(log_pdf), ncol = 1)) 
 px$log_pdf <- log_pdf
-px$grid <- find_grid
+px$grid <- list(grid)
 px <- px %>%
   rowwise() %>% 
   mutate(px = list(exp(log_pdf(grid)))) %$% px
@@ -46,90 +46,22 @@ transform_metrics <- trials_transform %>%
       log_value = TRUE
     )
   ) %>%
-  dplyr::mutate(
+  mutate(
     nEval = metrics$nEval,
     ESS = metrics$EffSamp,
     time = metrics$Time,
     draws = metrics$Draws,
-    thin = length(draws)/ESS,
+    thin = (length(draws)/ESS) * 10,
     thinDraws = list(LaplacesDemon::Thin(draws, thin)),
     samplesThin = length(thinDraws),
-    truncThinDraws = list(
-      sample(unlist(thinDraws), ifelse(samplesThin <= sampleSize, samplesThin, sampleSize))
-    ),
-    ksTest = ks.test(truncThinDraws, pgamma, shape = 2.5, rate = 1)$p.value,
+    ksTest = ks.test(thinDraws, pgamma, shape = 2.5, rate = 1)$p.value,
     KLD.JSD = list(LaplacesDemon::KLD(px = px, py = py)[c(4,6)]),
     KLD = KLD.JSD$sum.KLD.px.py,
     JSD = KLD.JSD$mean.sum.KLD
   ) %>%
-  dplyr::select(-c(metrics, KLD.JSD, truncThinDraws)) %>%
+  dplyr::select(-c(metrics, KLD.JSD, thinDraws)) %>%
   dplyr::mutate(SampPSec = ESS / time) %>%
   dplyr::relocate(samples, .after = time)
 
-# # the functions to compare against
-# dist_df <- data.frame(
-#   dist = rep("pgamma", 6),
-#   shape = c(1, 2, 2.5, 5, 3, 2.5),
-#   rate = c(1, 1, 5, 1, 1, 1)
-# )
-# 
-# list_hldr <- list(length = nrow(dist_df))
-# for (i in 1:nrow(dist_df)) {
-#   list_hldr[[i]] <- lapply(
-#     transform_metrics$thinDraws,
-#     FUN = ks.test,
-#     y = dist_df[i, 'dist'],
-#     shape = dist_df[i, 'shape'],
-#     rate = dist_df[i, 'rate']
-#   )
-# }
-# 
-# 
-# # saving the power test
-# pdf(file = "../../images_slice_sampler_comp/curve3_transform_power.pdf")
-# extract_pvals(list_hldr = list_hldr, dist_df = dist_df)
-# dev.off()
-
 # printing out metrics table
 saveRDS(transform_metrics,paste0("../../data/curve",curve_num,"_transform_metrics"))
-
-# evalTbl_stepping_out <- cbind(start_points_stepping_out, w_values_stepping_out) %>% round(.,1)
-pdf(file = "../../images_slice_sampler_comp/curve3_transform.pdf")
-curve(
-  fexp(f = lf, x = x),
-  col = 'red',
-  xlim = c(xlim_range[1], xlim_range[2]),
-  ylim = c(ylim_range[1], ylim_range[2]),
-  lwd = 2
-)
-lapply(transform_metrics$thinDraws, function(x) {
-  lines(density(x), col = adjustcolor('black'))
-})
-curve(
-  fexp(f = lf, x = x),
-  col = 'red',
-  xlim = c(xlim_range[1], xlim_range[2]),
-  ylim = c(ylim_range[1], ylim_range[2]),
-  lwd = 2,
-  add = TRUE
-)
-dev.off()
-
-
-transform_min_max <- results(transform_metrics, method = "Transform")
-
-transform_evaluation <- results_transform(transform_metrics)
-
-tab <- transform_evaluation %>%
-  xtable::xtable(caption = "This table shows the method of sampling, settings for the tuning parameters, average number of effective samples taken per second, the perecentage of samples that matched the target disribution according to a kolmogorov-Smirnov test, and the Kullback-Leibler divergence.",
-                 digits = c(0,0,0,2,2,0,2))
-
-xtable::print.xtable(tab, file = '../../images_slice_sampler_comp/curve3_transform_evaluation.tex')
-
-
-rm(
-  # list_hldr,
-  # dist_df,
-  trials_transform,
-  transform_metrics
-)
