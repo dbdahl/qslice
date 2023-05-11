5fc7d9191ea5cb4224e86c5412e802fa0d905f3b
smbjohnson <smbjohnson98@gmail.com>
2023-05-11 13:40:37 -0600

    Small changes to setup file

--- sam_comparison/setup.R
+++ sam_comparison/setup.R
@@ -1,5 +1,4 @@
-rm(list=ls())
-# setwd("~/cucumber/sam_comparison")
+# loading in libraries
 library(magrittr)
 library(tidyverse)
 library(LaplacesDemon) # thin function, KLD, JSD
@@ -8,201 +7,123 @@ library(utils)
 source("../sam_comparison_functions.R")
 
 
-reps <- 10
+reps <- 1
 
 fexp <- function(f, x) exp(f(x))
-samples <- rep(5000,reps)
-auto.cor.lim <- 0.10
-sampleSize <- 10000
+samples <- rep(500000,reps)
+sampleSize <- 5000
 
-# function that extracts pvalues when calculating power
-extract_pvals <- function(list_hldr, dist_df) {
-  counter <- 1
-  pvalue_list <- list(length = nrow(dist_df))
-  temp_df <- data.frame(matrix(nrow = nrow(dist_df) * reps, ncol = ncol(dist_df)))
-  for(j in 1:nrow(dist_df)) {
-    pvalue <- vector(length = reps)
-    for(i in 1:reps) {
-      pvalue[[i]] <- list_hldr[[j]][[i]]$p.value
-      temp_df[counter,] <- dist_df[j,]
-      counter <- counter + 1
-    }
-    pvalue_list[[j]] <- pvalue
-  }
-  cbind(temp_df, pVal = unlist(pvalue_list)) %>%
-    unite('Distribution', X1:X3) %>%
-    ggplot(aes(x = pVal, color = Distribution)) +
-    geom_histogram() +
-    facet_wrap(~Distribution) +
-    theme(
-      legend.position = 'none'
-    )
-}
-
-
-# returns the results of any slice sampling method in a nice table
-results <- function(df, method){
-  if(method == 'Stepping Out'){
-    param_settings <- df %>%
-      group_by(x,w) %>%
-      summarise(Samples = mean(SampPSec),
-                Accepted = mean(ksTest > 0.05)) %>%
-      mutate(Method = 'Stepping Out')
-
-    param_settings_df <- data.frame('Tuning Parameters' = c('x,w'),
-                         'Values' = paste(param_settings$x,',',param_settings$w),
-                         'Samples' = param_settings$Samples,
-                         'Accepted' = param_settings$Accepted)
-
-    return(param_settings_df %>%
-      separate('Values',into = c('x','w'), sep = ',') %>%
-      mutate(x = paste('$x =',x,'$'),
-             w = paste('$w =',w,'$')) %>%
-      unite('Settings',x:w, sep = ',') %>%
-      mutate('Method' = 'Stepping Out') %>%
-      relocate('Method', .before = Settings) %>%
-      select(-Tuning.Parameters))
-  }
-  if(method == 'Latent'){
-    param_settings <- df %>%
-      group_by(x,s,rate) %>%
-      summarise(Samples = mean(SampPSec),
-                Accepted = mean(ksTest > 0.05)) %>%
-      mutate(Method = 'Latent')
-    
-    param_settings_df <- data.frame('Tuning Parameters' = c('x,s,rate'),
-               'Values' = paste(param_settings$x,',',param_settings$s,',',param_settings$rate),
-               'Samples' = param_settings$Samples,
-               'Accepted' = param_settings$Accepted)
-    
-    return(param_settings_df %>%
-             separate('Values',into = c('x','s','rate'), sep = ',') %>%
-             mutate(x = paste('$x =',x,'$'),
-                    s = paste('$s =',s,'$'),
-                    rate = paste('$\\text{rate} =',rate,'$')) %>%
-             unite('Settings',x:rate, sep = ',') %>%
-             mutate('Method' = 'Latent') %>%
-             relocate('Method', .before = Settings) %>%
-             select(-Tuning.Parameters))
-  }
-  if(method == 'GESS'){
-    param_settings <- df %>%
-      group_by(x,mu,sigma,df) %>%
-      summarise(Samples = mean(SampPSec),
-                Accepted = mean(ksTest > 0.05)) %>%
-      mutate(Method = 'GESS')
-
-    param_settings_df <- data.frame('Tuning Parameters' = c('x,mu,sigma,df'),
-                         'Values' = paste(param_settings$x,',',param_settings$mu,',',param_settings$sigma,',',param_settings$df),
-                         'Samples' = param_settings$Samples,
-                         'Accepted' = param_settings$Accepted)
-
-    return(param_settings_df%>%
-             separate('Values',into = c('x','mu','sigma','df'), sep = ',') %>%
-             mutate(x = paste('$x =',x,'$'),
-                    mu = paste('$\\mu =',mu,'$'),
-                    sigma = paste('$\\sigma =',sigma,'$'),
-                    df = paste('$\\text{df} =',df,'$')) %>%
-             unite('Settings',x:df, sep = ',') %>%
-             mutate('Method' = 'GESS') %>%
-             relocate('Method', .before = Settings) %>%
-             select(-Tuning.Parameters))
-  }
-  if(method == 'Transform'){
-    param_settings <- df %>%
-      group_by(x,inv_cdf) %>%
-      summarise(Samples = mean(SampPSec),
-                Accepted = mean(ksTest > 0.05)) %>%
-      mutate(Method = 'Transform')
-
-    param_settings_df <- data.frame('Tuning Parameters' = c('x,inv_cdf'),
-                         'Values' = paste(param_settings$x,',',param_settings$inv_cdf),
-                         'Samples' = param_settings$Samples,
-                         'Accepted' = param_settings$Accepted)
-
-    return(param_settings_df %>%
-             separate('Values',into = c('x','inv_cdf'), sep = ' ,') %>%
-             mutate(x = paste('$x =',x,'$'),
-                    inv_cdf = paste('$\\text{inv cdf} =$',inv_cdf)) %>%
-             unite('Settings',x:inv_cdf, sep = ',') %>%
-             mutate('Method' = 'Transform') %>%
-             relocate('Method', .before = Settings) %>%
-             select(-c(Tuning.Parameters)))
-  }
-  if(method == 'Random Walk') {
-    param_settings <- df %>%
-      group_by(x,c) %>%
-      summarise(Samples = mean(SampPSec),
-                Accepted = mean(ksTest > 0.05)) %>%
-      mutate(Method = 'Random Walk')
-
-    param_settings_df <- data.frame('Tuning Parameters' = c('x,c'),
-                                    'Values' = paste(param_settings$x,',',param_settings$c),
-                                    'Samples' = param_settings$Samples,
-                                    'Accepted' = param_settings$Accepted)
-
-    return(param_settings_df %>%
-             separate('Values',into = c('x','c'), sep = ' ,') %>%
-             mutate(x = paste('$x =',x,'$'),
-                    c = paste('$c =$',c)) %>%
-             unite('Settings',x:c, sep = ',') %>%
-             mutate('Method' = 'Random Walk') %>%
-             relocate('Method', .before = Settings) %>%
-             select(-c(Tuning.Parameters)))
-  }
-}
-
-# this function returns the results of the transform slice sampler in a nice table
-results_transform <- function(df) {
-  param_settings <- df %>%
-    group_by(x,inv_cdf) %>%
-    summarise(KLD = mean(KLD),
-              JSD = mean(JSD),
-              Samples = mean(SampPSec),
-              Accepted = mean(ksTest > 0.05)) %>%
-    mutate(Method = 'Transform')
-
-  param_settings_df <- data.frame('Tuning Parameters' = c('x,inv_cdf'),
-                                  'Values' = paste(param_settings$x,',',param_settings$inv_cdf),
-                                  'KLD' = param_settings$KLD,
-                                  'JSD' = param_settings$JSD,
-                                  'Samples' = param_settings$Samples,
-                                  'Accepted' = param_settings$Accepted)
-
-  return(param_settings_df %>%
-           separate('Values',into = c('x','inv_cdf'), sep = ' ,') %>%
-           mutate(x = paste('$x =',x,'$'),
-                  inv_cdf = paste('$\\text{inv cdf} =$',inv_cdf)) %>%
-           unite('Settings',x:inv_cdf, sep = ',') %>%
-           mutate('Method' = 'Transform') %>%
-           relocate('Method', .before = Settings) %>%
-           select(-c(Tuning.Parameters)))
-}
 
 # this function finds the second derivative of function f at point x
 second_derivative <- function( x, h = 1e-5, f ) {
+  
   num <- f(x + h) - 2*f(x) + f(x - h)
   denom <- h^2
+  
   num/denom
 }
 
 # this function provides a lapace approximation for a curve lf
-laplace_approx <- function(lf, h = 1e-5, init){
+laplace_approx <- function(lf, h = 1e-5, init, inflate = TRUE){
   fit <- optim(init, lf, control=list(fnscale=-1), method = 'BFGS')
-  sd <- sqrt(-solve(second_derivative(fit$par, h = h, f = lf))) * 3
+  if(inflate){
+    sd <- sqrt(-solve(second_derivative(fit$par, h = h, f = lf))) * 3
+  } else {
+    sd <- sqrt(-solve(second_derivative(fit$par, h = h, f = lf)))
+  }
   return(
       c(log_pdf = function(x) {dnorm(x, mean = fit$par, sd = sd, log = TRUE)},
-      inv_cdf = function(u, lower.tail = TRUE) {qnorm(u, mean = fit$par, sd = sd, lower.tail = lower.tail)})
+      inv_cdf = function(u, lower.tail = TRUE) {qnorm(u, mean = fit$par, sd = sd, lower.tail = lower.tail)},
+      mu = fit$par,
+      sigma = sd)
   )
 }
 
-options(xtable.format.args = list(big.mark = ","), # separates large numbers using a ,
-        # xtable.size = "\\tiny", # makes the font tiny to fit into the overleaf presentation
-        xtable.append = FALSE, # replaces the table
-        xtable.table.placement = 'h', # table placment h is for "here"
-        xtable.caption.placement = 'bottom', # places the caption at the bottom
-        xtable.include.rownames = FALSE, # makes it so there are now row names
-        # xtable.hline.after = seq(from = -1, to = nrow(tab), by = 1), # adds a line after every row
-        xtable.sanitize.text.function = function(x){x}, # makes it so latex shows in the column names
-        adjustcolor.alpha.f = 0.02 #adjustcolor('black', alpha.f = 0.95) # changes the opacity of the lines comparing target to samples
-)
+# this function provides a cauchy approximation for a curve lf
+cauchy_approx <- function(lf, h = 1e-5, init, scaleInflation = 1) {
+  temp <- laplace_approx(lf, h = h, init = init, inflate = FALSE)
+  return(
+    c(log_pdf = function(x) {dcauchy(x, location = temp$mu, scale = temp$sigma)},
+      inv_cdf = function(u) {qcauchy(u, location = temp$mu, scale = temp$sigma)},
+      loc = temp$mu,
+      scale = scaleInflation*temp$sigma)
+  )
+}
+
+
+
+
+# helpful functions
+# truncated cauchy
+dcauchy_trunc <- function(x, loc, sc, lb=-Inf, ub=Inf, log=FALSE) {
+  stopifnot(lb < ub)
+  
+  denom <- pcauchy(ub, loc=loc, scale=sc) - pcauchy(lb, loc=loc, scale=sc)
+  
+  if (log) {
+    out <- dcauchy(x, loc=loc, sc=sc, log=TRUE) - log(denom)
+    out[which(x <= lb)] = -Inf
+    out[which(x >= ub)] = -Inf
+  } else {
+    out <- dcauchy(x, loc=loc, sc=sc) / denom
+    out[which(x <= lb)] = 0.0
+    out[which(x >= ub)] = 0.0
+  }
+  
+  out
+}
+
+# give draws and will return scale and location for a cauchy
+fit_trunc_Cauchy <- function(y, lb=-Inf, ub=Inf) {
+  
+  llik <- function(params, y, lb, ub) {
+    loc <- params[1]
+    sc <- params[2]
+    
+    stopifnot(all(y < ub))
+    stopifnot(all(y > lb))
+    
+    if (sc > 0.0) {
+      llik <- dcauchy_trunc(y, loc=loc, sc=sc, lb=lb, ub=ub, log=TRUE)
+      out <- sum(llik)
+    } else {
+      out <- -Inf
+    }
+    
+    out
+  }
+  
+  fit <- optim(par = c(mean(y), sd(y)), fn=llik, control=list(fnscale=-1),
+               y = y, lb = lb, ub = ub)
+  
+  list(loc = fit$par[1], sc = fit$par[2], lb = lb, ub = ub, fit = fit)
+}
+
+# this function will return the log pdf and inv cdf for a cauchy
+pseudo_Cauchy <- function(loc, sc, lb=-Inf, ub=Inf) {
+  
+  plb <- pcauchy(lb, loc=loc, sc=sc)
+  pub <- pcauchy(ub, loc=loc, sc=sc)
+  normc <- pub - plb
+  
+  pseudo_log_pdf <- function(x) dcauchy(x, loc=loc, sc=sc, log=TRUE) - log(normc)
+  pseudo_inv_cdf <- function(u) qcauchy(plb + u*normc) * sc + loc
+  
+  list(pseudo_log_pdf = pseudo_log_pdf,
+       pseudo_inv_cdf = pseudo_inv_cdf,
+       loc = loc, sc = sc)
+}
+
+# this function returns a Laplace t approximation with log pdf and inv cdf
+lapproxt <- function(lf, init, sc_adj = 1.0, lb = -Inf, ub = Inf, ...) {
+  
+  fit <- optim(par = init, fn = lf, control = list(fnscale = -1), method = 'BFGS')
+  loc <- fit$par
+  hessian <- second_derivative( x = loc, h = 1e-5, f = lf )
+  hessian_f <- hessian * exp(fit$value) # hessian of original f
+  sc <- sc_adj / sqrt(-hessian_f)
+  out <- pseudo_Cauchy(loc = loc, sc = sc, lb = lb, ub = ub)
+  out[["fit"]] <- fit
+  
+  out
+}
