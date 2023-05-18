b4994fdbbbeac9884b0caa96534f07b496b892c5
smbjohnson <smbjohnson98@gmail.com>
2023-05-17 18:03:55 -0600

    Plotting each plot from the stepping out procedure

--- gprior/gpriorSteppingOutExploration.R
+++ gprior/gpriorSteppingOutExploration.R
@@ -7,6 +7,50 @@ library(doParallel)
 library(mcmc)
 library(coda)
 
+source('formatingFunctions.R') # needed to create the traceplots
+
+slice_sampler_stepping_out <- function (x, target, w, max = 0, log = TRUE) 
+{
+  nEvaluations <- 0
+  f <- function(x) {
+    nEvaluations <<- nEvaluations + 1
+    target(x)
+  }
+  fx <- f(x)
+  y <- if (isTRUE(log)) {
+    log(runif(1)) + fx
+  }
+  else {
+    runif(1) * fx
+  }
+  L <- x - runif(1) * w
+  R <- L + w
+  if (!is.finite(max)) {
+    while (y < f(L)) L <- L - w
+    while (y < f(R)) R <- R + w
+  }
+  else if (max > 0) {
+    J <- floor(runif(1) * max)
+    K <- max - 1 - J
+    while (J > 0 && y < f(L)) {
+      L <- L - w
+      J <- J - 1
+    }
+    while (K > 0 && y < f(R)) {
+      R <- R + w
+      K <- K - 1
+    }
+  }
+  repeat {
+    x1 <- L + runif(1) * (R - L)
+    if (y < f(x1)) 
+      return(list(x = x1, x0 = x, l = L, r = R, y = y, nEvaluations = nEvaluations))
+    if (x1 < x) 
+      L <- x1
+    else R <- x1
+  }
+}
+
 data(mtcars)
 
 attach(mtcars)
@@ -42,7 +86,11 @@ g <- g_max / 2
 # number of samples to be taken
 n_samples <- 10000
 # pre allocating memory
-samples <- list(beta = matrix(0.0, nrow = n_samples, ncol = length(beta)), psi = numeric(n_samples), g = numeric(n_samples), time = numeric(1))
+samples <- list(beta = matrix(0.0, nrow = n_samples, ncol = length(beta)),
+                psi = numeric(n_samples), g = numeric(n_samples),
+                time = numeric(1), w = numeric(1), nEval = numeric(n_samples),
+                x0 = numeric(n_samples), y = numeric(n_samples),
+                l = numeric(n_samples), r = numeric(n_samples))
 # setting up chain storage
 chainSamples <- vector('list', length = nChains)
 chainSamples <- lapply(chainSamples, \(list) samples)
@@ -56,8 +104,15 @@ f <- \(g) {
 yy <- sapply(xx, f)
 plot(xx,yy)
 
+w <- c(45, 50, 55, 60, 65)
+
+unlink('explorationPlots/*', recursive = TRUE)
+
+sapply(w, FUN = \(w) dir.create(paste0('explorationPlots/w',w)))
+
 # collecting the samples
-# output <- foreach( chain = seq_along(chainSamples) ) %do% {
+output <- foreach( chain = seq_along(chainSamples) ) %do% {
+# for( chain in seq_along(chainSamples) ) {
   time <- system.time({
     for (i in seq_len(n_samples)) {
       q <- g / (1 + g)
@@ -68,54 +123,77 @@ plot(xx,yy)
       residuals <- y - X %*% t(beta)
       b_n <- b_0 + 0.5 * sum(residuals^2)
       psi <- rgamma(1, a_n, b_n)
-      if(i %% 1000 == 0) {
+      # updating g
+      temp <- slice_sampler_stepping_out(g, \(g) {
+        beta_cov <- g / psi * inv_XtX
+        dmvnorm(beta, beta_0, beta_cov, log = TRUE) + ifelse(0 < g && g < g_max, 0, -Inf)
+      }, w = w[chain], log = TRUE, max = Inf)
+      # plotting the update process
+      if(i == 1) { par(mfrow = c(4,5)); pdf(file = paste0('explorationPlots/w',w[chain],'/',i,'iter.pdf')) }
+      if(i %% 1000 == 0 | i %in% c(1,2,3,4,5)) {
         f <- \(g) {
           beta_cov <- g / psi * inv_XtX
           exp(dmvnorm(beta, beta_0, beta_cov, log = TRUE) + ifelse(0 < g && g < g_max, 0, -Inf))
         }
         yy <- sapply(xx, f)
-        plot(xx,yy)
-        browser()
+        expy <- exp(temp$y)
+        plot(xx,yy, main = paste0('g=',round(g),', iter=',i),cex = 0.5)
+        points(x = temp$x, y = expy, col = 'red') # new point
+        points(x = temp$x0, y = expy, col = 'dodgerblue') # original point
+        points(x = c(temp$l,temp$r), y = c(expy,expy), col = 'black', pch = 3)
         # readline(prompt = 'continue?')
       }
-      g <- cucumber::slice_sampler_stepping_out(g, \(g) {
-        beta_cov <- g / psi * inv_XtX
-        dmvnorm(beta, beta_0, beta_cov, log = TRUE) + ifelse(0 < g && g < g_max, 0, -Inf)
-      }, w = 100, log = TRUE)$x
-      chainSamples[[1]]$beta[i,] <- beta
-      chainSamples[[1]]$psi[i] <- psi
-      chainSamples[[1]]$g[i] <- g
+      if(i == n_samples) dev.off()
+      # saving all the variables in the plotting process
+      g <- temp$x
+      chainSamples[[chain]]$beta[i,] <- beta
+      chainSamples[[chain]]$psi[i] <- psi
+      chainSamples[[chain]]$g[i] <- g
+      chainSamples[[chain]]$nEval[i] <- temp$nEvaluations
+      chainSamples[[chain]]$x0[i] <- temp$x0
+      chainSamples[[chain]]$y[i] <- temp$y
+      chainSamples[[chain]]$l[i] <- temp$l
+      chainSamples[[chain]]$r[i] <- temp$r
     }
   })
-#   chainSamples[[chain]]$time <- time['user.self']
-# }
+  chainSamples[[chain]]$time <- time['user.self']
+  chainSamples[[chain]]$w <- w[chain]
+}
 
 
 # evaluation of samples
 
-traceplot <- function(matrix, mainTitle = NULL) {
-  n <- ncol(matrix)
-  # finding eff sample size of samples
-  effSize <- round(sum(apply(matrix, 2, effectiveSize)))
-  # finding Rhat
-  chainList <- mcmc.list()
-  for (i in 1:n) chainList[[i]] <- mcmc(matrix[,i])
-  Rhat <- round(gelman.diag(chainList)$psrf[1,1],3)
-  # making trace plot
-  color <- RColorBrewer::brewer.pal(n, 'Set2')
-  plot(matrix[,1], type = 'l', col = color[i], main = paste0(mainTitle, ' ESS=',effSize, ' Rhat=',Rhat),
-       ylab = '')
-  for( i in 2:n ) {
-    lines(matrix[,i], type = 'l', col = color[i])
-  }
-}
-
+# time
+time <- sapply(chainSamples, \(list) list$time)
 
 # g parameter
 gSamples <- sapply(chainSamples, \(list) list$g)
 
 traceplot(gSamples,'g')
 plot(density(c(gSamples)), main = 'g')
+
+
+tst <- sapply(1:ncol(gSamples) , FUN = \(i) {
+  col <- gSamples[,i]
+  effSize <- round(coda::effectiveSize(col))
+  sampPSec <- round(effSize/time[i])
+  plot(density(col), main = paste0('w=',w[i],', SampPSec=',sampPSec))
+  fiveSummary <- round(summary(col))
+  plot(col, type = 'l', main = paste0('w=',w[i],', min=',fiveSummary[1],', mean=',fiveSummary[4],', max=',fiveSummary[5]))
+  data.frame(w = w[i], sampPSec = sampPSec, est = round(mean(col),2), lwrCred = round(quantile(col, probs = 0.025)), uprCred = round(quantile(col, probs = 0.975)) )
+})
+
+t(tst)
+
+# number of evaluations
+nEvals <- sapply(chainSamples, \(list) list$nEval)
+
+sapply(1:ncol(nEvals), FUN = \(i) {
+ col <- nEvals[,i]
+ plot(col, main = paste0('w=',w[i]))
+}
+)
+
 
 # psi parameter
 psiSamples <- sapply(chainSamples, \(list) list$psi)
@@ -132,9 +210,8 @@ sapply(betaSamples, traceplot)
 par(mfrow = c(3,4))
 sapply(betaSamples, \(list) plot(density(c(list)), main = 'beta'))
 
-# time
-sapply(chainSamples, \(list) list$time)
 
-saveRDS(chainSamples, file = 'data/steppingOut.rds')
+
+# saveRDS(chainSamples, file = 'data/steppingOut.rds')
 
 
