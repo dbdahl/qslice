8d8749dd45875f5364f8b13af0870a7ae6ba084e
smbjohnson <smbjohnson98@gmail.com>
2023-05-19 13:09:18 -0600

    Every 1000 a new psueduo prior is fitting using dr.heiner optim

--- gprior/gpriorTransformEveryIterLaplace.R
+++ gprior/gpriorTransformEveryIterLaplace.R
@@ -19,8 +19,6 @@ n <- length(y)
 detach(mtcars)
 
 nChains <- 5
-cl <- makeCluster(nChains)
-registerDoParallel(cl)
 
 # fm <- lm(y ~ -1 + X)
 # summary(fm)
@@ -60,11 +58,14 @@ f <- \(g) {
 xx <- seq(from = 0.01, to = 200, length.out = 1000)
 yy <- sapply(xx,f)
 
-plot(xx,yy)
+# plot(xx,yy)
 
 # this doesn't work because there is no maximum
-psuedoFit <- lapproxt(f, 10, lb = 0)
+psuedoFit <- lapproxt(f = f, init = 10, lb = 0)
 psuedoTarget <- pseudo_Cauchy(loc = psuedoFit$loc, sc = psuedoFit$sc, lb = 0, name = 'Laprox')
+
+cl <- makeCluster(nChains)
+registerDoParallel(cl)
 
 # collecting the samples
 output <- foreach( chain = seq_along(chainSamples) ) %do% {
@@ -78,17 +79,26 @@ output <- foreach( chain = seq_along(chainSamples) ) %do% {
       residuals <- y - X %*% t(beta)
       b_n <- b_0 + 0.5 * sum(residuals^2)
       psi <- rgamma(1, a_n, b_n)
-      f <- \(g) {
-        beta_cov <- g / psi * inv_XtX
-        exp(dmvnorm(beta, beta_0, beta_cov, log = TRUE) + ifelse(0 < g && g < g_max, 0, -Inf))
+      ##
+      if(i %% 1000 == 0) {
+        newg <- chainSamples[[chain]]$g[1:i]
+        if(min(newg) < 0) browser(text = 'A g is less than 0')
+        psuedoFit <- opt_Cauchy_auc_data(newg, lb = 0)
+        psuedoTarget <- pseudo_Cauchy(loc = psuedoFit$loc, sc = psuedoFit$sc, lb = psuedoFit$lb, ub = psuedoFit$ub)
       }
-      psuedoFit <- lapproxt(f, 10, lb = 0)
-      psuedoTarget <- pseudo_Cauchy(loc = psuedoFit$loc, sc = psuedoFit$sc, lb = 0, name = 'Laprox')
-      browser()
+      ##
       g <- cucumber::slice_sampler_transform(x = g, target = \(g) {
         beta_cov <- g / psi * inv_XtX
         dmvnorm(beta, beta_0, beta_cov, log = TRUE) + ifelse(0 < g && g < g_max, 0, -Inf)
       }, pseudo_log_pdf = psuedoTarget$pseudo_log_pdf, pseudo_inv_cdf = psuedoTarget$pseudo_inv_cdf)$x
+      ##
+      # f <- \(g) {
+      #   beta_cov <- g / psi * inv_XtX
+      #   exp(dmvnorm(beta, beta_0, beta_cov, log = TRUE) + ifelse(0 < g && g < g_max, 0, -Inf))
+      # }
+      # psuedoFit <- lapproxt(f = f, init = 10, lb = 0)
+      # psuedoTarget <- pseudo_Cauchy(loc = psuedoFit$loc, sc = psuedoFit$sc, lb = 0, name = 'Laprox')
+      ##
       chainSamples[[chain]]$beta[i,] <- beta
       chainSamples[[chain]]$psi[i] <- psi
       chainSamples[[chain]]$g[i] <- g
