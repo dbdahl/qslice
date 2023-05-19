f7051813760184d417e24e269acdb092d5448d56
smbjohnson <smbjohnson98@gmail.com>
2023-05-19 13:13:07 -0600

    Adding a the metrics for the g parameter

--- gprior/summary.R
+++ gprior/summary.R
@@ -23,8 +23,25 @@ gResults <- sapply(1:length(results), FUN = \(i) {
 })
 
 
-t(gResults) %>% 
+temp <- t(gResults) %>% 
   data.frame() %>% 
-  dplyr::relocate(method, .before = 'lwrCrd')
+  dplyr::relocate(method, .before = 'lwrCrd') %>% 
+  dplyr::arrange(desc(unlist(SampPSec)))
 
+temp
 
+gDensities <- lapply(data, FUN = \(list) {
+  g <- sapply(1:length(list), FUN = \(i) {
+    list[[i]]$g
+  })
+  c(g)
+})
+
+colors <- RColorBrewer::brewer.pal(length(gDensities), name = 'Paired')
+names <- names(gDensities) %>% stringr::str_remove('data/') %>% stringr::str_remove('.rds')
+
+plot(density(gDensities[[1]]), col = colors[1], main = 'Density Plots of g', ylim = c(0,0.05), lty = 1)
+for(i in 2:length(gDensities)) {
+  lines(density(gDensities[[i]]), col = colors[i], lty = i)
+}
+legend(x = 60, y = 0.048, legend = names, col = colors, lty = 1:length(gDensities), cex = 0.8)
