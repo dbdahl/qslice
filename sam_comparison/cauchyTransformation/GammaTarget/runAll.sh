b4433cb89036417b99807b222011fa1c809a3425
smbjohnson <smbjohnson98@gmail.com>
2023-05-31 15:31:11 -0600

    changing the runAll script so its more efficient

--- sam_comparison/cauchyTransformation/GammaTarget/runAll.sh
+++ sam_comparison/cauchyTransformation/GammaTarget/runAll.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-Rscript gamma_gess.R > log/logGess.txt 2>&1 &
-Rscript gamma_latent.R > log/logLatent.txt 2>&1 &
-Rscript gamma_rand_walk.R > log/logRandWalk.txt 2>&1 &
-Rscript gamma_stepping_out.R > log/logSteppingOut.txt 2>&1 &
-Rscript gamma_transform.R > log/logTransform.txt 2>&1 &
+for file in gamma_[glrst]*.R
+do
+    fileName="${file/.R/.txt}"
+    Rscript $file > log/$fileName 2>&1 &
+done
+
--- sam_comparison/cauchyTransformation/NormTarget/runAll.sh
+++ sam_comparison/cauchyTransformation/NormTarget/runAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Rscript norm_gess.R > log/logGess.txt 2>&1 &
-Rscript norm_latent.R > log/logLatent.txt 2>&1 &
-Rscript norm_rand_walk.R > log/logRandWalk.txt 2>&1 &
-Rscript norm_stepping_out.R >log/logSteppingOut.txt 2>&1 &
-Rscript norm_transform.R > log/logTransform.txt 2>&1 &
+for file in norm_[glsrt]*.R
+do
+	fileName="${file/.R/.txt}"
+	Rscript $file > log/$fileName 2>&1 &
+done
